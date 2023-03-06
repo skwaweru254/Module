01b175bdeb7d01035559f29a46e74c20e8e1c452
skwaweru254 <skwaweru254@localhost.localdomain>
2023-03-06 16:13:20 +0300

    We are here

---
+++ importmodule.py
@@ -0,0 +1,11 @@
+import _ctypes
+
+import mymodule
+
+from mymodule import baby
+
+print ("Samantha you are",baby["age"], "years old")
+
+print("Your age is ",mymodule.add(20+5,0))
+
+print("Her name is",mymodule.name('Samantha'))
---
+++ mymodule.py
@@ -0,0 +1,38 @@
+# Function valentine
+
+def valentine(name):
+    """
+    A Python docstring is a string used to document
+    a Python module, class, function or method, so programmers 
+    can understand what it does without having to read the details 
+    of the implementation.
+    """
+    print("Hello, " + name)
+
+# Dictionary baby
+
+baby = {
+  "name": "Samantha",
+  "age": 25,
+  "country": "Kenya",
+  "Tribe": "Mijikenda",
+  "Lover" : "Simon"
+}
+
+def add(x,y):
+  """
+  The fucnction with parameter x and y is used to return
+  their addition.
+  """
+
+  return(x+y)
+
+def name(first):
+   """
+  The fucnction with parameter first is used to return
+  their addition.
+  """
+   
+   return first
+
+
